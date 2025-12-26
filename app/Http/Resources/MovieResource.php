bbb76bda8817e40a0b93a0f14e2e80f3a5f58fe7
Alexandre Moretti <alemoretti10@gmail.com>
2025-12-25 21:27:24 -0300

    feat: create CharacterResource and MovieResource

---
+++ app/Http/Resources/MovieResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\DTOs\MovieDto;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MovieResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        /** @var MovieDto $dto */
+        $dto = $this->resource;
+
+        return [
+            'title' => $dto->title,
+            'opening_crawl' => $dto->openingCrawl,
+        ];
+    }
+}
