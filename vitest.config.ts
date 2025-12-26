6b685c67f1a11a890aac8a479572da28e5dbc84c
Alexandre Moretti <alemoretti10@gmail.com>
2025-12-26 12:59:14 -0300

    fix: removes laravel-vite-plugin, not necessary

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            refresh: true,
-        }),
-        react(),
-    ],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
     test: {
         globals: true,
         environment: 'jsdom',
