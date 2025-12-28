f647a46c2a3cdded75da105f4382890bff1193e1
Alexandre Moretti <alemoretti10@gmail.com>
2025-12-28 16:23:10 -0300

    feat: handle backend pagination

--- app/Http/Requests/SearchRequest.php
+++ app/Http/Requests/SearchRequest.php
@@ -16,6 +16,7 @@ class SearchRequest extends FormRequest
         return [
             'query' => ['required', 'string', 'min:1', 'max:255'],
             'type' => ['required', 'in:people,movies'],
+            'page' => ['sometimes', 'integer', 'min:1'],
         ];
     }
 }
